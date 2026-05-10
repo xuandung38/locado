d5acf1af1064871349a4fdf1e35ffc578fe775af
HXD.VN <me@hxd.vn>
2026-05-10 16:20:45 +0700

    Remove trap for temporary directory cleanup
    Remove cleanup trap for temporary directory.

--- public/install.sh
+++ public/install.sh
@@ -112,7 +112,6 @@ info "Downloading $FILENAME..."
 
 # Create temp directory
 TMPDIR=$(mktemp -d)
-trap "rm -rf $TMPDIR" EXIT
 
 # Download and extract
 if ! curl -sL "$URL" -o "$TMPDIR/$FILENAME"; then
